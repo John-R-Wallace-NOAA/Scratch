35d2045ef4969786b9d120c362c7d763c60e0145
John Wallace <John.Wallace@noaa.gov>
2016-10-15 17:53:39 -0700

    Update sf2.R

--- sf2.R
+++ sf2.R
@@ -1,6 +1,8 @@
 sf2  <- function() {
 
-install.packages("rgdal", repos="http://R-Forge.R-project.org")
+# install.packages("rgdal", repos="http://R-Forge.R-project.org")
+
+devtools::install_version("rgdal", version="1.1-9")
 
 
 }
