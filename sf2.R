f2fbbdcc4750cc36426a40304766f60f06aeb8bb
John Wallace <John.Wallace@noaa.gov>
2016-10-16 20:29:23 -0700

    Update sf2.R

--- sf2.R
+++ sf2.R
@@ -2,7 +2,13 @@ sf2  <- function() {
 
 # install.packages("rgdal", repos="http://R-Forge.R-project.org")
 
-devtools::install_version("rgdal", version="1.1-8")
+# devtools::install_version("rgdal", version="1.1-8")
+
+
+gitAFile("https://raw.githubusercontent.com/John-R-Wallace/R-ToolBox/master/match.f", run = run, show = show)
+
+
+
 
 
 }
