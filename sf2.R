f23852c3b593a4c87572949d0cc9b3494e0dec08
John Wallace <John.Wallace@noaa.gov>
2016-10-16 21:43:58 -0700

    Update sf2.R

--- sf2.R
+++ sf2.R
@@ -5,7 +5,7 @@ sf2  <- function() {
 # devtools::install_version("rgdal", version="1.1-8")
 
 
-gitAFile("https://raw.githubusercontent.com/John-R-Wallace/R-ToolBox/master/R/match.f.R/", run = run, show = show)
+gitAFile("https://raw.githubusercontent.com/John-R-Wallace/R-ToolBox/master/R/match.f.R/", run = F, show = T)
 
 
 # gitAFile("https://rawgit.com/John-R-Wallace/R-ToolBox/master/R/match.f.R", run = run, show = show)
